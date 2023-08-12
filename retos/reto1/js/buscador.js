bfe4230c1a630ffc9a009408cd8acedd69f90a41
yianxd <jean4520rodriguez@gmail.com>
2023-08-11 22:27:43 -0500

    la verdad no se que tanto hice xd

---
+++ retos/reto1/js/buscador.js
@@ -0,0 +1,11 @@
+document.addEventListener("keyup",e=>{
+    if(e.target.matches("#buscador")){
+        document.querySelectorAll('.articulo').forEach(
+            articulo=>{
+                articulo.textContent.toLowerCase().includes(e.target.value)
+                ? articulo.classList.remove('filtro')
+                : articulo.classList.add('filtro');
+            })
+    }
+})
+
