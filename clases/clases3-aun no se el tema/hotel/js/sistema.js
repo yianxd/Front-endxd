9564b24b7f99b94d31cd52f9161d61b85652c08d
yianxd <jean4520rodriguez@gmail.com>
2023-09-04 17:04:59 -0500

    avance de hoy: quedo funcional, pero no bonito xd

---
+++ clases/clases3-aun no se el tema/hotel/js/sistema.js
@@ -0,0 +1,12 @@
+function seleccionar(name){
+    var link = document.querySelectorAll('header nav a');
+
+    link[0].className="";
+    link[1].className="";
+    link[2].className="";
+    link[3].className="";
+    link[4].className="";
+    link[5].className="";
+
+    link.className="select"
+}
